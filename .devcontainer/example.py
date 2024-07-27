833ab7c1a9dc35fe9b7c060fdd9197066f597c58
Siva Suriya D <161417113+SivaSuriya23@users.noreply.github.com>
2024-07-27 22:56:00 +0530

    Update example.py

--- .devcontainer/example.py
+++ .devcontainer/example.py
@@ -5,7 +5,7 @@ import psycopg2
 
 # Database connection details
 db_user = 'postgres'
-db_password = 'password'
+db_password = 'password'  # Replace with your actual password
 db_host = 'localhost'
 db_port = '5432'
 db_name = 'bus_details'
